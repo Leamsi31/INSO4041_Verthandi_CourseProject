ad1a6526d1300a0bbfd57c22ada032ef79561485
EdwinC1339 <edwin.camuy@upr.edu>
2022-10-25 09:37:39 -0400

    Added getters and setters to student

--- src/node/student.js
+++ src/node/student.js
@@ -1,6 +1,11 @@
 import 'user';
 
 class Student extends User {
+
+    #coursingYear
+    #major
+    #tags
+
     constructor(userconfig) {
         // Super constructor will initialize all the account credentials
         super(userconfig);
@@ -9,6 +14,21 @@ class Student extends User {
         major = userconfig.major;
         // Tags are initialized as empty array, will populate with questionnaire results.
         tags = [];
+    }
 
+    getCoursingYear() {
+        return this.coursingYear;
+    }
+
+    getMajor() {
+        return this.major;
+    }
+
+    getTags() {
+        return this.tags;
+    }
+
+    setTags(tags) {
+        this.tags = tags;
     }
 };
