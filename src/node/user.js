686ae0d6a98d849663d88fd10476153c04d8f155
EdwinC1339 <edwin.camuy@upr.edu>
2022-10-25 09:41:59 -0400

    Added getters and setters for user class

--- src/node/user.js
+++ src/node/user.js
@@ -2,8 +2,35 @@
 User class provides the basic structure of a user, organizer and student class will inherit from user.
 */
 class User {
+    
+    #username
+    #hash
+
     constructor(userconfig) {
         this.username = userconfig.username;
         this.hash = userconfig.hash;
+    }
+
+    getConfig() {
+        const config = {
+            username: this.username,
+            hash: this.hash
+        }
+
+        return config;
+    }
+
+    getUsername() {
+        return this.username;
+    }
+
+    getHash() {
+        return this.hash;
+    }
+
+    // We deliberately leave out a setter method for the username field because it is an immutable field.
+
+    setHash(hash) {
+        this.hash = hash;
     }
 };
