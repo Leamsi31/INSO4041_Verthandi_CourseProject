7fe10ae9c72bc13be273e9fef3d378beab79f725
EdwinC1339 <edwin.camuy@upr.edu>
2022-10-28 23:17:01 -0400

    Fixed auth page Uses react instead of jquery for switching between signup and signin

--- src/website/src/pages/Auth.jsx
+++ src/website/src/pages/Auth.jsx
@@ -1,79 +1,76 @@
 
-import React from 'react';
+import React, { Component } from 'react';
 import './Auth.css'
 
+export default class Auth extends Component {
+  constructor() {
+    super();
+    this.state = {
+      signIn: true,
+      signUp: false
+    }
+  }
+  render() {
+    console.log(this.state.signIn);
 
-
-
-
-
-
-
-
-
-
-function Auth() {
-    console.log("hoopla")
-    const switchers = [...document.querySelectorAll('.switcher')]
-
-switchers.forEach(item => {
-	item.addEventListener('click', function() {
-		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
-		this.parentElement.classList.add('is-active')
-	})
-})
-return (
-<div className="SignupFormBody">
-    <section className="forms-section">
-  <h1 className="section-title">Login</h1>
-  <div className="forms">
-    <div className="form-wrapper is-active">
-      <button type="button" className="switcher switcher-login">
-        Login
-        <span className="underline"></span>
-      </button>
-      <form className="form form-login">
-        <fieldset>
-          <legend>Please, enter your email and password for login.</legend>
-          <div className="input-block">
-            <label htmlFor="login-email">E-mail</label>
-            <input   id="login-email" type="email" required></input>
+    return (
+      <div className="SignupFormBody">
+          <section className="forms-section">
+        <h1 className="section-title">Login</h1>
+        <div className="forms">
+          <div className={"form-wrapper".concat(this.state.signIn ? " is-active" : "")}>
+            <button type="button" onClick={() => {
+              this.setState({signIn: true, signUp: false}); 
+              }} className="switcher switcher-login">
+              Login
+              <span className="underline"></span>
+            </button>
+            <form className="form form-login">
+              <fieldset>
+                <legend>Please, enter your email and password for login.</legend>
+                <div className="input-block">
+                  <label htmlFor="login-email">E-mail</label>
+                  <input   id="login-email" type="email" required></input>
+                </div>
+                <div className="input-block">
+                  <label htmlFor="login-password">Password</label>
+                  <input id="login-password" type="password" required></input>
+                </div>
+              </fieldset>
+              <button type="submit" className="btn-login">Login</button>
+            </form>
           </div>
-          <div className="input-block">
-            <label htmlFor="login-password">Password</label>
-            <input id="login-password" type="password" required></input>
+          <div className={"form-wrapper".concat(this.state.signUp ? " is-active" : "")}>
+            <button type="button" onClick={() => {
+              this.setState({signIn: false, signUp: true}); 
+              }} className="switcher switcher-signup">
+              Sign Up
+              <span className="underline"></span>
+            </button>
+            <form className="form form-signup">
+              <fieldset>
+                <legend>Please, enter your email, password and password confirmation for sign up.</legend>
+                <div className="input-block">
+                  <label htmlFor="signup-email">E-mail</label>
+                  <input id="signup-email" type="email" required></input>
+                </div>
+                <div className="input-block">
+                  <label htmlFor="signup-password">Password</label>
+                  <input id="signup-password" type="password" required></input>
+                </div>
+                <div className="input-block">
+                  <label htmlFor="signup-password-confirm">Confirm password</label>
+                  <input id="signup-password-confirm" type="password" required></input>
+                </div>
+              </fieldset>
+              <button type="submit" className="btn-signup">Continue</button>
+            </form>
           </div>
-        </fieldset>
-        <button type="submit" className="btn-login">Login</button>
-      </form>
-    </div>
-    <div className="form-wrapper">
-      <button type="button" className="switcher switcher-signup">
-        Sign Up
-        <span className="underline"></span>
-      </button>
-      <form className="form form-signup">
-        <fieldset>
-          <legend>Please, enter your email, password and password confirmation for sign up.</legend>
-          <div className="input-block">
-            <label htmlFor="signup-email">E-mail</label>
-            <input id="signup-email" type="email" required></input>
-          </div>
-          <div className="input-block">
-            <label htmlFor="signup-password">Password</label>
-            <input id="signup-password" type="password" required></input>
-          </div>
-          <div className="input-block">
-            <label htmlFor="signup-password-confirm">Confirm password</label>
-            <input id="signup-password-confirm" type="password" required></input>
-          </div>
-        </fieldset>
-        <button type="submit" className="btn-signup">Continue</button>
-      </form>
-    </div>
-  </div>
-</section>
-</div>
+        </div>
+      </section>
+      </div>
+      );
+    }
     
         // <div>  	
         //     <input type="checkbox" id="chk" aria-hidden="true"></input>
@@ -110,6 +107,5 @@ return (
   
     
 
-);
+
 }
-export default Auth;
