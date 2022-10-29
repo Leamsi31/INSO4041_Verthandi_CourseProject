dd7db0ae6028f32aba15202e281b685ce8ee4009
EdwinC1339 <edwin.camuy@upr.edu>
2022-10-29 11:31:19 -0400

    Drafted tag selection component

---
+++ src/website/src/components/TagSelection.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from React;
+
+export default class TagSelection extends Component {
+    constructor() {
+        super();
+        this.state = {
+            allTags : [],
+            selectedTags : []
+        };
+    }
+
+    async getTags() {
+        const rawResponse = await fetch('/tags');
+        this.setState({
+            tags : await rawResponse.json(),
+            selectedTags : []
+        });
+    }
+
+    componentDidMount() {
+        this.getTags();
+    }
+
+    render() {
+        const allTagsDisplay = this.state.allTags.map((tag) =>{
+            return (
+                <li key={tag}>{tag}</li>
+            );
+        });
+        const selectedTagsDisplay = this.state.selectedTags.map((tag) =>{
+            return (
+                <li key={tag}>{tag}</li>
+            );
+        });
+        return (
+            <div className="tag-selection container">
+                <div className="tag-group">
+                    {allTagsDisplay}
+                </div>
+                <div className="tag-group">
+                    {}
+                </div>
+            </div>
+        )
+    }
+}
