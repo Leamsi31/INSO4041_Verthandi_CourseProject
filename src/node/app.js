8cd83c0b40141406cbd6bfd5487fa1737fd3340e
AlonsoUriarte14 <diego.santiago6@upr.edu>
2022-11-26 07:53:35 -0400

    added changes to app.js in backend

--- src/node/app.js
+++ src/node/app.js
@@ -8,8 +8,8 @@ import query from './query.js'
 import Student from './student.js';
 import Activity from './activity.js';
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
+
+
 const passport = require('passport');
 const flash = require('connect-flash');
 const session = require('express-session');
@@ -18,11 +18,44 @@ const UserRouter = require("./controller/User")
 const app = express();
 const jsonParser = bodyParser.json();
 
+const path = require("path");
+
 // Passport Config
 require('./config/passport')(passport);
 
+// react setup?
+app.set('view engine', 'html');
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+  );
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+  });
+
+// handle all user login / register stuff 
+app.use("/Auth", UserRouter);
 
 app.post('/questionnaire', jsonParser, (req, res) => {
     const { id } = req.body;
@@ -48,18 +81,16 @@ app.get('/tags', (req, res) => {
 
     res.json(tags);
 });
-// handle all user login / register stuff 
-app.use("/Auth", UserRouter);
 
-// const dummyConfig = {
-//     username : "Queko Jones",
-//     hash: "graciaskiara",
-//     coursingYear: 5,
-//     major: "FINA",
-//     tags: ["Social", "Política"]
-// };
-// const dummy = new Student(dummyConfig);
-// query.signUpStudent(dummy);
+ const dummyConfig = {
+    username : "pee Jones",
+    hash: "graciaskiara",
+    coursingYear: 5,
+    major: "FINA",
+    tags: ["Social", "Política"]
+}; 
+const dummy = new Student(dummyConfig); 
+query.signUpStudent(dummy);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
